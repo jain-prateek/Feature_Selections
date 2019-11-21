205860366cacc13f7454f95e7ccbe0e64c89bffc
Prateek Jain <hiprtk@gmail.com>
2019-11-21 15:11:22 +0530

    Correlation Matrix with Heatmap

---
+++ Correlation_Matrix_Heatmap.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+data = pd.read_csv("D://Blogs//train.csv")
+X = data.iloc[:,0:20]  #independent columns
+y = data.iloc[:,-1]    #target column i.e price range
+#get correlations of each features in dataset
+corrmat = data.corr()
+top_corr_features = corrmat.index
+plt.figure(figsize=(20,20))
+#plot heat map
+g=sns.heatmap(data[top_corr_features].corr(),annot=True,cmap="RdYlGn")
